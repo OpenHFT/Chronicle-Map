cdbb38e12d5fbd8c7eb31801147c91495b966f69
Rob Austin <rob.austin@boundedbuffer>
2014-09-29 15:52:51 +0100

    HCOLL-134  removed unwanted field

--- src/main/java/net/openhft/chronicle/map/TcpReplicator.java
+++ src/main/java/net/openhft/chronicle/map/TcpReplicator.java
@@ -46,7 +46,7 @@ class TcpReplicator extends AbstractChannelReplicator implements Closeable {
 
     private static final Logger LOG = LoggerFactory.getLogger(TcpReplicator.class.getName());
     private static final int BUFFER_SIZE = 0x100000; // 1MB
-    final AtomicBoolean forceBootstrap = new AtomicBoolean();
+
     private final SelectionKey[] selectionKeysStore = new SelectionKey[Byte.MAX_VALUE + 1];
     // used to instruct the selector thread to set OP_WRITE on a key correlated by the bit index in the
     // bitset
