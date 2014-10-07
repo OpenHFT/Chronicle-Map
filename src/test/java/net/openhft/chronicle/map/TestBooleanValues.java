8be62872b8aa2e7bae5dbf208ed9add141bbb03b
Rob Austin <rob.austin@boundedbuffer>
2014-10-07 14:16:50 +0100

    issue reported on http://stackoverflow.com/questions/26219313/strange-npe-from-chronicle-map-toy-code

--- src/test/java/net/openhft/chronicle/map/TestBooleanValues.java
+++ src/test/java/net/openhft/chronicle/map/TestBooleanValues.java
@@ -13,6 +13,9 @@ public class TestBooleanValues {
 
     @Test
     @Ignore
+    /**
+     * see isusue http://stackoverflow.com/questions/26219313/strange-npe-from-chronicle-map-toy-code
+     */
     public void testTestBooleanValues() throws IOException, InterruptedException {
         ChronicleMap<Integer, Boolean> map = ChronicleMapBuilder.of(Integer.class, Boolean.class).create();
         map.put(7, true);
