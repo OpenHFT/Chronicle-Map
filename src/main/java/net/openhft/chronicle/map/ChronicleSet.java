a5bf4f8448da8b151bc63385450db922e876e185
leventov <leventov@ya.ru>
2014-10-14 13:58:28 +0400

    HCOLL-136 HCOLL-137 HCOLL-164 Extracted ChronicleHash abstraction and updated ChronicleMap docs

--- src/main/java/net/openhft/chronicle/map/ChronicleSet.java
+++
@@ -1,8 +0,0 @@
-package net.openhft.chronicle.map;
-
-import java.io.Closeable;
-import java.util.Set;
-
-public interface ChronicleSet<E> extends Set<E>, Closeable {
-    public long longSize();
-}
