d3dd3f5ee7bcf7f507c5e715d6f715f1dccb4edc
Rob Austin <rob.austin@boundedbuffer>
2014-11-14 12:07:43 +0000

    HCOLL-81 removed - @FunctionalInterface

--- src/main/java/net/openhft/chronicle/java8/Function.java
+++ src/main/java/net/openhft/chronicle/java8/Function.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
  * @param <R> the type of the result of the function
  * @since 1.8
  */
-@FunctionalInterface
+
 public interface Function<T, R> extends Serializable {
 
     /**
