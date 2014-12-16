e87f2b4c111a7ce107a21fefc3537be6ecdb21c0
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-12-16 23:17:45 +0000

    HCOLL-242 change to use ->     Object o = isKey ? newKeyInstance() : newValueInstance();

--- src/main/java/net/openhft/chronicle/map/VanillaChronicleMap.java
+++ src/main/java/net/openhft/chronicle/map/VanillaChronicleMap.java
@@ -821,7 +821,7 @@ class VanillaChronicleMap<K, KI, MKI extends MetaBytesInterop<K, ? super KI>,
                         case "chronicle-key":
 
                             if (kClass.getCanonicalName().startsWith("net.openhft.lang.values")) {
-                                return to$$Native(reader, vClass);
+                                return to$$Native(reader, vClass, true);
                             } else {
                                 long keySize = keySizeMarshaller.readSize(buffer);
                                 ThreadLocalCopies copies = keyReaderProvider.getCopies(null);
@@ -831,7 +831,7 @@ class VanillaChronicleMap<K, KI, MKI extends MetaBytesInterop<K, ? super KI>,
                         case "chronicle-value":
 
                             if (vClass.getCanonicalName().startsWith("net.openhft.lang.values")) {
-                                return to$$Native(reader, vClass);
+                                return to$$Native(reader, vClass, false);
                             } else {
                                 long valueSize = valueSizeMarshaller.readSize(buffer);
                                 ThreadLocalCopies copies = valueReaderProvider.getCopies(null);
@@ -859,9 +859,9 @@ class VanillaChronicleMap<K, KI, MKI extends MetaBytesInterop<K, ? super KI>,
                     } else return (E) unmarshallingContext.convertAnother(null, clazz);
                 }
 
-                private Object to$$Native(HierarchicalStreamReader reader, final Class aClass) {
+                private Object to$$Native(HierarchicalStreamReader reader, final Class aClass, final boolean isKey) {
 
-                    Object o = DataValueClasses.newDirectInstance(aClass);
+                    final Object o = isKey ? newKeyInstance() : newValueInstance();
                     try {
                         for (Method $$nativeMethod : o.getClass().getMethods()) {
                             if ($$nativeMethod.getName().equals("setValue") && $$nativeMethod.getParameterTypes()
