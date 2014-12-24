8894206ab45751913d477de5c9a0c408976b10bb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-12-24 00:11:45 +0000

    removed System.out

--- src/test/java/net/openhft/chronicle/map/CHMUseCasesTest.java
+++ src/test/java/net/openhft/chronicle/map/CHMUseCasesTest.java
@@ -313,8 +313,6 @@ public class CHMUseCasesTest {
         try (ChronicleMap<CharSequence, char[]> map = newInstance(builder)) {
             map.put("Key", expected);
 
-            char[] actual = map.get("Key");
-            System.out.println(actual);
             assertEquals(Chars.asList(expected), Chars.asList(map.get("Key")));
             mapChecks();
         }
