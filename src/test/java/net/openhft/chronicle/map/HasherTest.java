c407f9c10136b226af0da5a70896e014bea0cefd
leventov <leventov@ya.ru>
2015-01-24 11:37:35 +0700

    #15 Fixed bug in Hasher

---
+++ src/test/java/net/openhft/chronicle/map/HasherTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 Higher Frequency Trading http://www.higherfrequencytrading.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.openhft.chronicle.map;
+
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import net.openhft.chronicle.hash.hashing.Hasher;
+
+public class HasherTest {
+    public static void main(String[] args) {
+
+        HashFunction hashFunction = Hashing.murmur3_128();
+        for (int i=0;i<10;i++)
+        {
+            String key = "010758403"+String.format("%06d",i)+"S-INJFIX_SLE";
+            long hashCode = Hasher.hash(key.getBytes());
+            long guavaHashCode = hashFunction.hashBytes(key.getBytes()).asLong();
+            System.out.println("<"+key+"> => "+hashCode+" "+guavaHashCode);
+        }
+    }
+}
