b9302248a2575f6f6c2994a9ff176ef1ff5a0545
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-02-02 09:28:52 +0000

    now not closing on a  - AsynchronousCloseException e

--- src/main/java/net/openhft/chronicle/map/StatelessChronicleMap.java
+++ src/main/java/net/openhft/chronicle/map/StatelessChronicleMap.java
@@ -42,6 +42,7 @@ import java.lang.reflect.Field;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.channels.AsynchronousCloseException;
 import java.nio.channels.SocketChannel;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -1151,6 +1152,9 @@ class StatelessChronicleMap<K, V> implements ChronicleMap<K, V>, Closeable, Clon
         try {
 
             return blockingFetchThrowable(timeoutTime, transactionId);
+        } catch (AsynchronousCloseException e) {
+            LOG.error("name=" + name, e);
+            throw new RuntimeException(e);
         } catch (IOException e) {
             close();
             throw new IORuntimeException(e);
