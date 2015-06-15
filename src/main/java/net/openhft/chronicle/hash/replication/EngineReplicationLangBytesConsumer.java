66609b6cd2e0d2b5b97df5daf0338fb7b4b54dd1
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-06-15 18:45:29 +0100

    CE-57 improved Engine to Engine replication

--- src/main/java/net/openhft/chronicle/hash/replication/EngineReplicationLangBytesConsumer.java
+++ src/main/java/net/openhft/chronicle/hash/replication/EngineReplicationLangBytesConsumer.java
@@ -1,11 +1,12 @@
 package net.openhft.chronicle.hash.replication;
 
 import net.openhft.chronicle.map.EngineReplicationLangBytes;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Rob Austin
  */
 public interface EngineReplicationLangBytesConsumer {
 
-    void set(EngineReplicationLangBytes engineReplicationLangBytes);
+    void set(@NotNull EngineReplicationLangBytes engineReplicationLangBytes);
 }
