19e4fce3d04d89dd73036f427381007d27385316
leventov <leventov@ya.ru>
2015-07-19 06:13:09 +0300

    Add StatelessClientDuplicateKeyTest (see #33)

---
+++ src/test/java/net/openhft/chronicle/map/StatelessClientDuplicateKeyTest.java
@@ -0,0 +1,65 @@
+/*
+ *     Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.openhft.chronicle.map;
+
+import net.openhft.chronicle.hash.replication.TcpTransportAndNetworkConfig;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static net.openhft.chronicle.map.ChronicleMapStatelessClientBuilder.*;
+
+public class StatelessClientDuplicateKeyTest {
+
+    @Test
+    public void statelessClientDuplicateKeyTest() throws IOException, InterruptedException, ExecutionException {
+        ChronicleMap<Integer, Integer> map =
+                ChronicleMapBuilder.of(Integer.class, Integer.class)
+                .replication((byte) 1, TcpTransportAndNetworkConfig.of(8080))
+                .create();
+
+        map.put(42, 1);
+
+        final ChronicleMap<Integer, Integer> client =
+                createClientOf(new InetSocketAddress("localhost", 8080));
+
+        ExecutorService runner = new ThreadPoolExecutor(16, 256, 300L, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<Runnable>());
+
+        class Run implements Callable<Integer> {
+
+            @Override
+            public Integer call() {
+                return client.get(42);
+            }
+        }
+        Collection<Run> items = new ArrayList<>(1000);
+        for (int i = 0; i < 1000; i++) {
+            items.add(new Run());
+        }
+        List<Future<Integer>> futures = runner.invokeAll(items);
+        for (Future<Integer> future : futures) {
+            Assert.assertEquals((Integer) 1, future.get());
+        }
+    }
+}
