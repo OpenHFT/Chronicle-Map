4fb7141a3473114fb9a987dc78fc821482f5e905
leventov <leventov@ya.ru>
2015-11-25 20:43:10 +0300

    Add EnumMarshallable (this file should be added in commit fe77012)

---
+++ src/main/java/net/openhft/chronicle/hash/serialization/impl/EnumMarshallable.java
@@ -0,0 +1,37 @@
+/*
+ *      Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU Lesser General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.openhft.chronicle.hash.serialization.impl;
+
+import net.openhft.chronicle.bytes.IORuntimeException;
+import net.openhft.chronicle.core.util.ReadResolvable;
+import net.openhft.chronicle.wire.Marshallable;
+import net.openhft.chronicle.wire.WireIn;
+import net.openhft.chronicle.wire.WireOut;
+import org.jetbrains.annotations.NotNull;
+
+public interface EnumMarshallable<E extends Enum<E>> extends Marshallable, ReadResolvable<E> {
+
+    @Override
+    default void readMarshallable(@NotNull WireIn wireIn) throws IORuntimeException {
+        // shouldn't read fields, anyway readResolved later
+    }
+
+    @Override
+    default void writeMarshallable(@NotNull WireOut wireOut) {
+        // shouldn't read -> shouldn't write fields
+    }
+}
