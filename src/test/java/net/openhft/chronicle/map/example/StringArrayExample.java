4610a5eda038b7f9c22cc134247a0f591c3a4c7f
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-03-25 13:58:10 +0000

    added StringArrayExample

---
+++ src/test/java/net/openhft/chronicle/map/example/StringArrayExample.java
@@ -0,0 +1,64 @@
+package net.openhft.chronicle.map.example;
+
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+import net.openhft.chronicle.values.Array;
+import net.openhft.chronicle.values.MaxUtf8Length;
+import net.openhft.chronicle.values.Values;
+import org.junit.Test;
+
+/**
+ * an example of using a chronicle map with a string array like structure
+ */
+public class StringArrayExample {
+
+    private final CharSequenceArray charSequenceArray = Values.newHeapInstance(CharSequenceArray.class);
+
+    public interface CharSequenceArray {
+        @Array(length = 8)
+        void setCharSequenceWrapperAt(int index, CharSequenceWrapper value);
+
+        CharSequenceWrapper getCharSequenceWrapperAt(int index);
+    }
+
+    public interface CharSequenceWrapper {
+        void setCharSequence(@MaxUtf8Length(6) CharSequence idSource);
+
+        CharSequence getCharSequence();
+    }
+
+    @Test
+    public void simpleLoggingTest() {
+        ChronicleMap<Integer, CharSequenceArray> map = ChronicleMapBuilder
+                .of(Integer.class, CharSequenceArray.class)
+                .entries(100)
+
+                .create();
+        {
+            CharSequenceArray charSequenceArray = Values.newHeapInstance(CharSequenceArray.class);
+            map.put(1, charSequenceArray);
+        }
+        {
+            // change the value in the array
+            map.compute(1, this::setToHello);
+        }
+
+        {
+            // read the value
+            CharSequence charSequence = map.getUsing(1, charSequenceArray).getCharSequenceWrapperAt(1).getCharSequence();
+            System.out.println(charSequence);
+        }
+
+        {
+            // to string all the values
+            System.out.println(map.getUsing(1, charSequenceArray).toString());
+        }
+
+    }
+
+    private CharSequenceArray setToHello(final Integer integer, final CharSequenceArray charSequenceArray) {
+        charSequenceArray.getCharSequenceWrapperAt(1).setCharSequence("hello");
+        return charSequenceArray;
+    }
+
+}
