ddd66d8c31402bb9e166d44cdae29c94dca55407
Per Minborg <minborg@speedment.com>
2020-12-02 11:29:52 +0100

    Fix typo

--- src/main/java/net/openhft/chronicle/map/ChronicleMapBuilder.java
+++ src/main/java/net/openhft/chronicle/map/ChronicleMapBuilder.java
@@ -1013,7 +1013,7 @@ public final class ChronicleMapBuilder<K, V> implements
     long entries() {
         if (entries < 0) {
             throw new IllegalStateException("If in-memory Chronicle Map is created or persisted\n" +
-                    "to a file for the first time (i. e. not accessing existing file),\n" +
+                    "to a file for the first time (i. e. not accessing an existing file),\n" +
                     "ChronicleMapBuilder.entries() must be configured.\n" +
                     "See Chronicle Map 3 tutorial and javadocs for more information");
         }
