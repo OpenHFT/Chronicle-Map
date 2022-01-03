156287bd31af84668a31a2e7ea6e135c144c7649
Per Minborg <minborg@speedment.com>
2022-01-03 13:53:53 +0100

    Add reproducing tests, Fix #354

---
+++ src/test/java/net/openhft/chronicle/map/Issue354.java
@@ -0,0 +1,26 @@
+package net.openhft.chronicle.map;
+
+import net.openhft.chronicle.core.values.LongValue;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Issue354 {
+
+    @Test
+    public void reproduce() throws IOException {
+
+        final File file = new File("/tmp/issue354-map");
+
+        final ChronicleMapBuilder<LongValue, LongValue> builder = ChronicleMapBuilder.of(LongValue.class, LongValue.class)
+                .entries(5);
+
+        try (ChronicleMap<LongValue, LongValue> map = builder.createOrRecoverPersistedTo(file)) {
+
+        };
+
+
+    }
+
+}
---
+++ src/test/java/net/openhft/chronicle/map/Issue354b.java
@@ -0,0 +1,30 @@
+package net.openhft.chronicle.map;
+
+import net.openhft.chronicle.core.values.LongValue;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+public class Issue354b {
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void build_toFile() throws IOException {
+        String baseDirectory = testFolder.getRoot().toString();
+        File file = new File(baseDirectory, "chronicle.dat");
+        ChronicleMap<LongValue, LongValue> map = ChronicleMapBuilder.of(LongValue.class, LongValue.class)
+                .name("test")
+                .entries(5)
+                .createOrRecoverPersistedTo(file, true);
+
+        assertTrue(file.isFile());
+    }
+
+}
