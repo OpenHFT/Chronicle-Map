c1328267179c9bfec2e4fb6dc47d6307ff740027
Peter Lawrey <peter.lawrey@chronicle.software>
2026-04-29 13:25:45 +0100

    Track wrapped value cache reads

--- src/main/java/net/openhft/chronicle/map/impl/stage/data/bytes/WrappedValueBytesData.java
+++ src/main/java/net/openhft/chronicle/map/impl/stage/data/bytes/WrappedValueBytesData.java
@@ -48,6 +48,8 @@ public class WrappedValueBytesData<V> extends AbstractData<V> {
     private boolean wrappedValueBytesUsed = false;
     @Stage("CachedWrappedValue")
     private V cachedWrappedValue;
+    @Stage("CachedWrappedValue")
+    private boolean cachedWrappedValueRead = false;
 
     boolean nextInit() {
         return true;
@@ -100,6 +102,7 @@ public class WrappedValueBytesData<V> extends AbstractData<V> {
 
     private void initCachedWrappedValue() {
         cachedWrappedValue = innerGetUsing(cachedWrappedValue);
+        cachedWrappedValueRead = true;
     }
 
     @Override
